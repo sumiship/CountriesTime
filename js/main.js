de36246e209e129dfc8a7e8db053c57dfa57433e
sumiship <osumi.k.osumi.k@gmail.com>
2020-12-24 04:55:39 +0900

    modify 日付　曜日

--- js/main.js
+++ js/main.js
@@ -9,17 +9,18 @@
     }
   }
 
+
   let i=0;
 
   function clock() {
     let time = new Date();
     let month = addZero(time.getMonth());
-    let day = addZero(time.getDay());
+    let day = [ "日", "月", "火", "水", "木", "金", "土" ][time.getDay()];
     let date = addZero(time.getDate());
     let hour = addZero(time.getHours());
     let min = addZero(time.getMinutes());
     let sec = addZero(time.getSeconds());
-    let ret1 = month + "/" + day + " (" + date + ")";
+    let ret1 = month+1 + "/" + date + " (" + day + ")";
     let ret2 = hour + ":" + min + ":" + sec;
     document.getElementById('Japan1').textContent = ret1;
     document.getElementById('Japan2').textContent = ret2;
