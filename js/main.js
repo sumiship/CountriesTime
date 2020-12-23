1dc05559c4e3fe4b7fd50d49603351de86a3fadb
sumiship <osumi.k.osumi.k@gmail.com>
2020-12-24 07:23:02 +0900

    change to useful

--- js/main.js
+++ js/main.js
@@ -12,22 +12,35 @@
 
   let i=0;
 
-  function clock() {
-    let time = new Date();
+  function now(i) {
+    let now = (new Date()).getTime();
+    let nowGri = now + (new Date()).getTimezoneOffset()*60*1000;
+    return new Date(nowGri + i*60*60*1000);
+  }
+
+  function put(i) {
+    let time = now(i);
     let month = addZero(time.getMonth());
-    let day = [ "日", "月", "火", "水", "木", "金", "土" ][time.getDay()];
     let date = addZero(time.getDate());
+    let day = [ "日", "月", "火", "水", "木", "金", "土" ][time.getDay()];
     let hour = addZero(time.getHours());
     let min = addZero(time.getMinutes());
     let sec = addZero(time.getSeconds());
     let ret1 = month+1 + "/" + date + " (" + day + ")";
     let ret2 = hour + ":" + min + ":" + sec;
-    document.getElementById('Japan1').textContent = ret1;
-    document.getElementById('Japan2').textContent = ret2;
+    return [ret1, ret2]
+  }
+
+  function html(country, jisa) {
+    document.getElementById(`${country}1`).textContent = put(jisa)[0];
+    document.getElementById(`${country}2`).textContent = put(jisa)[1];
+  }
+
+  function clock() {
+    html("Japan",9);
     console.log(i);
     i += 1;
   }
-
 
 
   setInterval(clock,1000);
